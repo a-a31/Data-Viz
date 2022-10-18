ddbd8cbee4455d82c1b515df40e2a4122776c0a3
a-a31 <115389061+a-a31@users.noreply.github.com>
2022-10-18 15:07:44 +1100

    Update vegaLite.js

--- assignment2/vegaLite.js
+++ assignment2/vegaLite.js
@@ -1,14 +1,38 @@
-var vg_1 = "viz_1.json";
-vegaEmbed("#scatter_chart", vg_1)
+var vg_1 = "CHORO_MAP.json";
+vegaEmbed("#map_chart", vg_1)
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
   .catch(console.error);
 
 
-var vg_2 = "viz_2.json";
-vegaEmbed("#bubble_chart", vg_2)
+var vg_2 = "LOLLIPOP_OVERALL.json";
+vegaEmbed("#lollipop_top_10", vg_2)
 .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 })
 .catch(console.error);
+
+var vg_3 = "scatterplot_rating_value.json";
+vegaEmbed("#scatter_plot", vg_3)
+.then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+})
+.catch(console.error);
+
+var vg_4 = "Multi-chart.json";
+vegaEmbed("#multi_plot", vg_4)
+.then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+})
+.catch(console.error);
+
+var vg_5 = "potential.json";
+vegaEmbed("#potential", vg_5)
+.then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+})
+.catch(console.error);
+
+
+
